58b6babc7482c45731e245e9f498f3dabd52cf74
Mikołaj <mikolajrajczyk01@gmail.com>
2025-11-29 13:32:57 +0100

    test ci fix

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>MRajczyk.dev</h1>
+      <p>Work in progress!</p>
+      <p>Slowly but surely something magical will happen here!</p>
+    </>
+  );
+}
+
+export default App;
