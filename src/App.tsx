d2abfdcab6150310ec64e1707fd2b4b6942deae3
MRajczyk <mikolajrajczyk01@gmail.com>
2026-01-06 23:45:25 +0100

    test cd

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
       <h1>MRajczyk.dev</h1>
       <p>Work in progress!</p>
       <p>Slowly but surely something magical will happen here!</p>
+      <p>Stay tuned...</p>
     </>
   );
 }
